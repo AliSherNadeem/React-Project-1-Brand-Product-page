aef6a369bf499e57ac88a6ad9d57a1f5c02ace40
AliSherNadeem <alisher786389@gmail.com>
2024-02-14 12:12:14 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Hero from './components/Hero';
+import Navigation from './components/Navigation';
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <Hero />
+    </div>
+  );
+};
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,33 @@
+const Hero = () => {
+  return (
+    <main className="hero container">
+      <div className="hero-content">
+        <h1>YOUR FEET DESERVE THE BEST</h1>
+        <p>
+          YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.
+        </p>
+
+        <div className="hero-button">
+          <button>Shop Now</button>
+          <button className="secondary-btn">Category</button>
+        </div>
+
+        <div className="shopping">
+          <p>Also Available On</p>
+
+          <div className="Brand-icons">
+            <img src="./images/amazon.png" alt="amazaon icon" />
+            <img src="./images/flipkart.png" alt="flipkart icon" />
+          </div>
+        </div>
+      </div>
+
+      <div className="hero-image">
+        <img src="./images/shoe_image.png" alt="shoe image" />
+      </div>
+    </main>
+  );
+};
+export default Hero;
